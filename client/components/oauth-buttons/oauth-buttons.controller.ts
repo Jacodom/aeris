15380efc1abc54bb06f80901ad0b628748221379
Pablo <pablocordoba11@gmail.com>
2016-04-18 21:56:19 -0300

    Struct fullStack

---
+++ client/components/oauth-buttons/oauth-buttons.controller.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('appApp')
+  .controller('OauthButtonsCtrl', function($window) {
+    this.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
+  });
