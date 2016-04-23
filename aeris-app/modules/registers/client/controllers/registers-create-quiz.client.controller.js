79ffe0ddf0926b3d02ec0181243996629e7d0b4a
Jacobo Dominguez <dominguezjaco@gmail.com>
2016-04-23 12:00:56 -0300

    step one finished frontend

--- aeris-app/modules/registers/client/controllers/registers-create-quiz.client.controller.js
+++ aeris-app/modules/registers/client/controllers/registers-create-quiz.client.controller.js
@@ -13,6 +13,28 @@
     // Registers create quiz controller logic
     // ...
 
+
+
+    $scope.minSlider = {
+      value:0,
+      options:{
+        floor:0,
+        ceil:4,
+        step:1,
+        precision: 1
+      }
+    };
+
+    $scope.minSlider.cough = 0;
+    $scope.minSlider.breath = 0;
+    $scope.minSlider.sneezing = 0;
+    $scope.minSlider.wheezing = 0;
+    $scope.minSlider.nasal = 0;
+    $scope.minSlider.phlegm = 0;
+    $scope.minSlider.eyes = 0;
+    $scope.minSlider.snot = 0;
+
+
     init();
 
     function init() {
